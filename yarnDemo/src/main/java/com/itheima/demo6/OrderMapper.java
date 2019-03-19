d17bf5ac19308b3e3d6e64cfdd1315af2a8a802e
crazy chen <312497368@qq.com>
2019-03-19 21:54:09 +0800

    update project

--- yarnDemo/src/main/java/com/itheima/demo6/OrderMapper.java
+++ yarnDemo/src/main/java/com/itheima/demo6/OrderMapper.java
@@ -10,6 +10,7 @@ public class OrderMapper extends Mapper<LongWritable,Text,OrderBean,Text>{
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         String[] split = value.toString().split("\t");
-        context.write(new OrderBean(split[0],Double.valueOf(split[2])),value);
+        OrderBean k2 = new OrderBean(split[0], Double.valueOf(split[2]));
+        context.write(k2,value);
     }
 }
