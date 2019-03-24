88ac622b0ecc220776893664fe2bd2651737d53c
crazy chen <312497368@qq.com>
2019-03-24 21:59:38 +0800

    自定义解析JSON数据UDF

--- hiveDemo/src/main/java/com/itheima/demo2/JsonUDF.java
+++ hiveDemo/src/main/java/com/itheima/demo2/JsonUDF.java
@@ -1,27 +1,15 @@
 package com.itheima.demo2;
 
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FSDataInputStream;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
+import com.alibaba.fastjson.JSONObject;
 import org.apache.hadoop.hive.ql.exec.UDF;
 import org.apache.hadoop.io.Text;
-import org.testng.annotations.Test;
-
-import java.io.IOException;
 
 public class JsonUDF extends UDF {
-    public Text evaluate(String inputPath,String outputPath) throws IOException {
-        FileSystem fileSystem = FileSystem.get(new Configuration());
-        FSDataInputStream inputStream = fileSystem.open(new Path(inputPath));
-//        BufferedReader reader = new BufferedReader();
-
-        return null;
-    }
-    @Test
-    public void testPartition (){
-        for (int i = 1; i <5 ; i++) {
-            System.out.println(new Integer(i).hashCode() & Integer.MAX_VALUE);
+    public Text evaluate(Text line) {
+        if (line != null && !line.equals("")) {
+            MovieBean movieBean = JSONObject.parseObject(line.toString(), MovieBean.class);
+            return new Text(movieBean.toString());
         }
+        return null;
     }
 }
